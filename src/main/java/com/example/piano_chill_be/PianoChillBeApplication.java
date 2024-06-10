9eb6f98f76543b482e053a7464559c678fe848f9
ducnx1 <ducnx@beamewa.com.vn>
2024-06-10 15:08:56 +0700

    create project be

---
+++ src/main/java/com/example/piano_chill_be/PianoChillBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.piano_chill_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PianoChillBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PianoChillBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/piano_chill_be/PianoChillBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.piano_chill_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PianoChillBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
